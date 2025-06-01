be55a2c75de1b52940833943e5435dd00ff66cfc
B.Ribon <b.ribon@dynartio.com>
2025-06-01 21:25:40 +0200

    restructure folders

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## Information
 
+Country map images are produced by : https://github.com/ben10dynartio/osm-power-grid-map-analysis
+
+
+
 Each new realease contains a `docs.zip` which is a docs folder that have to be merged the docs folder of the website before mkdocs build.
 
 ## Technical notes
--- scripts/countrypages_generation/country_page_generation.py
+++ scripts/countrypages_generation/country_page_generation.py
@@ -1,40 +1,52 @@
 """
-This script generate country pages
+This script generate country pages for #ohmygrid website
 """
-
+import os
+import zipfile
 import pandas as pd
 from pathlib import Path
 import requests
 
-def get_wikicommons_image_url(image_name):
-    image_name = image_name.replace(" ", "_").replace("+", "%2B")
-    wikiurl = f"https://en.wikipedia.org/w/api.php?action=query&titles=File:{image_name}&prop=imageinfo&iiprop=url&format=json"
-    print("URL =", wikiurl )
-    r = requests.get(wikiurl)
-    r.raise_for_status()
-    print(r)
-    r = r.json()
-    for key, val in r["query"]["pages"].items():
-        return val["imageinfo"][0]["url"]
-
-df_wikidata = pd.read_csv("wikidata_countries_info_formatted.csv", na_filter = False)
-wikidata_dict = {row["codeiso2"]: row for row in df_wikidata.to_dict(orient='records')}
-print(df_wikidata.columns)
-
-df = pd.read_csv("openinframap_countries_info_brut.csv", na_filter = False)
-openinframap_dict = {row["codeiso2"]: row for row in df.to_dict(orient='records')}
-
-print(df.columns)
-
-df = pd.read_csv("powergrid_analysis_countries_info.csv", na_filter = False)
-powergrid_dict = {row["codeiso2"]: row for row in df.to_dict(orient='records')}
-print(df.columns)
-
 COUNTRY_PAGE_TEMPLATE = "country_page_template.md"
+DOCS_FOLDER_PATH = "../../docs/"
+PROGRESS_PAGE_PATH = DOCS_FOLDER_PATH + "progress.md"
+
 
 DESTINATION_DIRECTORY = Path(__file__).parent.parent.parent / "docs/countrypages"
 MAPS_IMAGES_DIRECTORY = Path(__file__).parent.parent.parent / "docs/images/maps_countries"
 print("Destination directory = ", DESTINATION_DIRECTORY)
+
+
+def zip_folder(folder_path, output_path=None):
+    if not os.path.isdir(folder_path):
+        raise ValueError(f"Le dossier spécifié n'existe pas : {folder_path}")
+
+    if output_path is None:
+        output_path = folder_path.rstrip(os.sep) + ".zip"
+
+    with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                abs_path = os.path.join(root, file)
+                rel_path = "docs/" + os.path.relpath(abs_path, folder_path)
+                zipf.write(abs_path, arcname=rel_path)
+
+    print(f"Dossier zippé avec succès : {output_path}")
+
+
+
+
+df_wikidata = pd.read_csv("wikidata_countries_info_formatted.csv", na_filter = False).set_index("codeiso2")
+#wikidata_dict = {row["codeiso2"]: row for row in df_wikidata.to_dict(orient='records')}
+print(df_wikidata.columns)
+
+df_openinframap = pd.read_csv("openinframap_countries_info_brut.csv", na_filter = False).set_index("codeiso2")
+#openinframap_dict = {row["codeiso2"]: row for row in df.to_dict(orient='records')}
+print(df_openinframap .columns)
+
+df_powergrid = pd.read_csv("powergrid_analysis_countries_info.csv", na_filter = False).set_index("codeiso2")
+#powergrid_dict = {row["codeiso2"]: row for row in df.to_dict(orient='records')}
+print(df_powergrid.columns)
 
 
 """ Following lines extract the country list per continent. You can use one of the following dict as a country list"""
@@ -47,9 +59,9 @@ SouthAmerica_light = {'AR': 'Argentina', 'BO': 'Bolivia', 'CL': 'Chile', 'CO': '
 Asia_light = {'AE': 'United Arab Emirates', 'AF': 'Afghanistan', 'AM': 'Armenia', 'AZ': 'Azerbaijan', 'BD': 'Bangladesh', 'BH': 'Bahrain', 'BN': 'Brunei', 'BT': 'Bhutan', 'ID': 'Indonesia', 'IL': 'Israel', 'IQ': 'Iraq', 'IR': 'Iran', 'JO': 'Jordan', 'JP': 'Japan', 'KG': 'Kyrgyzstan', 'KH': 'Cambodia', 'KP': 'North Korea', 'KR': 'South Korea', 'KW': 'Kuwait', 'KZ': 'Kazakhstan', 'LA': 'Laos', 'LB': 'Lebanon', 'LK': 'Sri Lanka', 'MM': 'Myanmar', 'MN': 'Mongolia', 'MV': 'Maldives', 'MY': 'Malaysia', 'NP': 'Nepal', 'OM': 'Oman', 'PH': 'Philippines', 'PK': 'Pakistan', 'PS': 'State of Palestine', 'QA': 'Qatar', 'SA': 'Saudi Arabia', 'SG': 'Singapore', 'SY': 'Syria', 'TH': 'Thailand', 'TJ': 'Tajikistan', 'TL': 'Timor-Leste', 'TM': 'Turkmenistan', 'TR': 'Turkey', 'TW': 'Taiwan', 'UZ': 'Uzbekistan', 'VN': 'Vietnam', 'YE': 'Yemen'}
 
 # No Brazil, China and India in light dict
-COUNTRY_LIST = { **Africa, **SouthAmerica_light, **Asia_light }
-COUNTRY_LIST = {'DK': 'Danish'}
-COUNTRY_LIST = {row["codeiso2"]: row["name"] for row in df_wikidata.to_dict(orient='records')}
+#COUNTRY_LIST = { **Africa, **SouthAmerica_light, **Asia_light }
+#COUNTRY_LIST = {'DK': 'Danish'}
+COUNTRY_LIST = {idx: df_wikidata.loc[idx].get("name") for idx in df_wikidata.index}
 SKIP_UNTIL = None # not in use yet
 
 #The following section are conditional, only for countries with map
@@ -69,32 +81,32 @@ Grid connectivity summary (nb of substations x nb of connections) :<br>{{POWER_G
 
 ## Building MD file &
 
-df_collector = []
+df_collector = [] # To collect each country informations
 for country_key in COUNTRY_LIST:
     template_data = {}
     template_data["COUNTRY_CODE"] = country_key
 
-    template_data["COUNTRY_NAME"] = wikidata_dict[country_key]["countryLabel"]
-    template_data["COUNTRY_CONTINENT"] = wikidata_dict[country_key]["continent"]
-    template_data["COUNTRY_POPULATION"] = wikidata_dict[country_key]["population"]
-    template_data["COUNTRY_AREA"] = wikidata_dict[country_key]["area_km2"]
-    template_data["COUNTRY_GDP"] = wikidata_dict[country_key]["gdp_bd"]
-    template_data["COUNTRY_OSM_REL_ID"] = wikidata_dict[country_key]["osm_rel_id"]
-    template_data["COUNTRY_WIKIDATA_ID"] = wikidata_dict[country_key]["osm_rel_id"]
-    template_data["COUNTRY_FLAG_IMAGE"] = get_wikicommons_image_url(wikidata_dict[country_key]["flag_image"])
-    template_data["COUNTRY_MAP_IMAGE"] = get_wikicommons_image_url(wikidata_dict[country_key]["locator_map"])
+    template_data["COUNTRY_NAME"] = df_wikidata.loc[country_key]["countryLabel"]
+    template_data["COUNTRY_CONTINENT"] = df_wikidata.loc[country_key]["continent"]
+    template_data["COUNTRY_POPULATION"] = df_wikidata.loc[country_key]["population"]
+    template_data["COUNTRY_AREA"] = df_wikidata.loc[country_key]["area_km2"]
+    template_data["COUNTRY_GDP"] = df_wikidata.loc[country_key]["gdp_bd"]
+    template_data["COUNTRY_OSM_REL_ID"] = df_wikidata.loc[country_key]["osm_rel_id"]
+    template_data["COUNTRY_WIKIDATA_ID"] = df_wikidata.loc[country_key]["osm_rel_id"]
+    template_data["COUNTRY_FLAG_IMAGE"] = df_wikidata.loc[country_key]["flag_image_url"]
+    template_data["COUNTRY_MAP_IMAGE"] = df_wikidata.loc[country_key]["locator_map_url"]
 
-    template_data["POWER_LINES_KM"] = openinframap_dict[country_key]["power_line_total_length"]
-    template_data["POWER_PLANTS_MW"] = openinframap_dict[country_key]["power_plant_output_mw"]
-    template_data["POWER_PLANTS_NB"] = int(openinframap_dict[country_key]["power_plant_count"])
+    template_data["POWER_LINES_KM"] = df_openinframap.loc[country_key]["power_line_total_length"]
+    template_data["POWER_PLANTS_MW"] = df_openinframap.loc[country_key]["power_plant_output_mw"]
+    template_data["POWER_PLANTS_NB"] = int(df_openinframap.loc[country_key]["power_plant_count"])
 
     template_data["POWER_SUBSTATIONS_NB"] = ''
     template_data["POWER_INTERCONNECTIONS_NB"] = ''
     template_data["POWER_GRID_CONNECTIVITY"] = ''
-    if powergrid_dict[country_key]["nb_substations"]:
-        template_data["POWER_SUBSTATIONS_NB"] = int(float(powergrid_dict[country_key]["nb_substations"]))
-        template_data["POWER_INTERCONNECTIONS_NB"] = int(float(powergrid_dict[country_key]["nb_international_connections"]))
-        template_data["POWER_GRID_CONNECTIVITY"] = powergrid_dict[country_key]["grid_connectivity"]
+    if df_powergrid.loc[country_key].get("nb_substations"):
+        template_data["POWER_SUBSTATIONS_NB"] = int(float(df_powergrid.loc[country_key]["nb_substations"]))
+        template_data["POWER_INTERCONNECTIONS_NB"] = int(float(df_powergrid.loc[country_key]["nb_international_connections"]))
+        template_data["POWER_GRID_CONNECTIVITY"] = df_powergrid.loc[country_key]["grid_connectivity"]
 
     template_data["SECTION_GRID_CONNECTIVITY"] = ""
     template_data["SECTION_PROGRESS_MAP"] = ""
@@ -102,6 +114,8 @@ for country_key in COUNTRY_LIST:
         template_data["SECTION_GRID_CONNECTIVITY"] = SECTION_GRID_CONNECTIVITY
         template_data["SECTION_PROGRESS_MAP"] = SECTION_PROGRESS_MAP
 
+    # Open COUNTRY_PAGE_TEMPLATE and replace "{{ }}" string with corresponding value
+    # It is done twice to manage SECTION_* values that are designed above
     with open(COUNTRY_PAGE_TEMPLATE, 'r', encoding='utf-8') as f:
         contenu = f.read()
         for key, val in template_data.items():
@@ -109,19 +123,36 @@ for country_key in COUNTRY_LIST:
         for key, val in template_data.items():
             contenu = contenu.replace(f'{{{{{key}}}}}', str(val))
 
+    # Export all md files for countries
     Path.mkdir(DESTINATION_DIRECTORY, exist_ok=True)
     with open(DESTINATION_DIRECTORY / f"{template_data['COUNTRY_NAME']}.md", 'w', encoding='utf-8') as f:
         f.write(contenu)
 
     df_collector.append(template_data)
 
+# Construct the string for the list of country (to be insterted on "progress.md" pages)
 df_collector = pd.DataFrame(df_collector)
-## Build
 lst_continent = df_collector['COUNTRY_CONTINENT'].unique().tolist()
 lst_continent.sort()
+mystr = ""
 for continent in lst_continent:
-    print("###", continent, "\n")
+    #print("###", continent, "\n")
+    mystr += "###" + continent + "\n"
     dft = df_collector[df_collector['COUNTRY_CONTINENT']==continent]
-    for row in dft.to_dict(orient='records'):
-        print(f"![Flag {row['COUNTRY_NAME']}]({row['COUNTRY_FLAG_IMAGE']}){{width=20px}} [{row['COUNTRY_NAME']}](countrypages/{row['COUNTRY_NAME']}.md) - ", end="")
-    print("\n")
+    for r in dft.to_dict(orient='records'):
+        # Country name link to the page, with a small flag on the left
+        mystr += (f"![Flag {r['COUNTRY_NAME']}]({r['COUNTRY_FLAG_IMAGE']}){{width=20px}} "
+                 f"[{r['COUNTRY_NAME']}](countrypages/{r['COUNTRY_NAME']}.md) - \n")
+        #print(mystr, end="")
+    mystr += "\n"
+    #print("\n")
+
+r = requests.get('https://raw.githubusercontent.com/open-energy-transition/Oh-my-Grid/refs/heads/main/docs/progress.md')
+newfile = r.text.replace("<!-- COUNTRY_LIST_INSERTION -->", mystr)
+with open(PROGRESS_PAGE_PATH,'w') as f:
+    f.write(newfile)
+
+
+# Exemple d'utilisation
+if __name__ == "__main__":
+    zip_folder(DOCS_FOLDER_PATH)
